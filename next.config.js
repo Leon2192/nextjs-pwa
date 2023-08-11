4fdc6b5741c961dbe79e1c5fb9d380644239e925
leon_2192 <leon.strokes@gmail.com>
2023-08-11 13:32:30 -0300

    next config

--- next.config.js
+++ next.config.js
@@ -1,18 +1,11 @@
+/** @type {import('next').NextConfig} */
+
 const withPWA = require('next-pwa')({
-    pwa: {
-        dest: 'public',
-        register: true,
-        skipWaiting: true,
-    },
-    reactStrictMode: true,
-    async rewrites() {
-        return [
-            {
-                source: '/service-worker.js',
-                destination: '/_next/static/service-worker.js',
-            },
-        ];
-    },
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
 });
 
-module.exports = withPWA;
+module.exports = withPWA({
+    reactStrictMode: true,
+});
