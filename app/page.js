9cfac2b1c81b1a85778752e03d57fda8b3793622
leon_2192 <leon.strokes@gmail.com>
2023-08-11 16:58:20 -0300

    fix apple

--- app/page.js
+++ app/page.js
@@ -38,7 +38,16 @@ export default function Home() {
       <div
         id="install-banner"
         className="fixed bottom-0 left-0 w-full bg-blue-500 text-white p-2 text-center cursor-pointer"
-        style={{ display: 'none' }}
+        style={{
+          display: 'none',
+          // Ajusta los estilos según tus preferencias
+          backgroundColor: '#0070f3',
+          color: 'white',
+          padding: '10px',
+          textAlign: 'center',
+          cursor: 'pointer',
+          zIndex: '1000',
+        }}
       >
         Instalar la aplicación
       </div>
