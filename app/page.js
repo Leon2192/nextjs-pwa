4bc4ae81f0b8f2deb27a19dcd16e9424ad3a05d5
leon_2192 <leon.strokes@gmail.com>
2023-08-11 18:13:55 -0300

    styles

--- app/page.js
+++ app/page.js
@@ -1,7 +1,10 @@
 'use client'
 import Head from 'next/head';
 import Image from 'next/image'
+import Landing from './components/Landing';
 import { useEffect } from 'react';
+import { ChakraProvider } from '@chakra-ui/provider';
+
 
 export default function Home() {
 
@@ -29,34 +32,30 @@ export default function Home() {
     }
   }, []);
 
-
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Head>
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-      </Head>
-      <div
-        id="install-banner"
-        className="fixed bottom-0 left-0 w-full bg-blue-500 text-white p-2 text-center cursor-pointer"
-        style={{
-          display: 'none',
-          // Ajusta los estilos según tus preferencias
-          backgroundColor: '#0070f3',
-          color: 'white',
-          padding: '10px',
-          textAlign: 'center',
-          cursor: 'pointer',
-          zIndex: '1000',
-        }}
-      >
-        Instalar la aplicación
-      </div>
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-      </div>
-    </main>
+    <ChakraProvider>
+      <main>
+        <Head>
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+        </Head>
+        <div
+          id="install-banner"
+          className="fixed bottom-0 left-0 w-full bg-blue-500 text-white p-2 text-center cursor-pointer"
+          style={{
+            display: 'none',
+            // Ajusta los estilos según tus preferencias
+            backgroundColor: '#0070f3',
+            color: 'white',
+            padding: '10px',
+            textAlign: 'center',
+            cursor: 'pointer',
+            zIndex: '1000',
+          }}
+        >
+          Instalar la aplicación
+        </div>
+        <Landing />
+      </main>
+    </ChakraProvider>
   )
 }
