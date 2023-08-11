b98fcfef41f260d1a43c43e23fba007d36fb38ae
leon_2192 <leon.strokes@gmail.com>
2023-08-11 10:21:53 -0300

    fix pwa test

--- next.config.js
+++ next.config.js
@@ -2,17 +2,8 @@ const withPWA = require('next-pwa')({
     dest: 'public',
     register: true,
     skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development'
-})
+});
 
-/** @type {import('next').NextConfig} */
-const path = require('path')
-
-const nextConfig = {
+module.exports = withPWA({
     reactStrictMode: true,
-    sassOptions: {
-        includePaths: [path.join(__dirname, 'styles')],
-    },
-}
-
-module.exports = nextConfig
+});
